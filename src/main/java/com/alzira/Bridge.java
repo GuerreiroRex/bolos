97329ee9fe80a38e69a9f5b428d94f5d3a19a673
GuerreiroRex <maik.masl@gmail.com>
2023-10-26 21:54:51 -0300

    Atualizando obanco de dados

--- src/main/java/com/alzira/Bridge.java
+++ src/main/java/com/alzira/Bridge.java
@@ -1,6 +1,8 @@
 package com.alzira;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import com.alzira.database.Database;
 import javafx.scene.web.WebEngine;
@@ -10,14 +12,31 @@ public class Bridge {
 
     public Bridge(WebEngine motor) {
         webEngine = motor;
-    }   
-    
+    }
+
     public void confirmarlogin(String usuario, String senha) throws IOException, SQLException, ClassNotFoundException {
-        
-        if (true) {
-            GUI.trocarTela("menu");
-        } /* else {
-            webEngine.executeScript("chamarInvalido()");
-        } */
+        Database.Conectar();
+        ResultSet restultado = Database.Ler("SELECT * FROM usuarios");
+
+        String db_username = null;
+        String db_senha = null;
+
+        while (restultado.next()) {
+            db_username = restultado.getString("username");
+            db_senha = restultado.getString("senha");
+
+            //System.err.println();
+
+            if (db_username.equals(usuario) & db_senha.equals(senha)) {
+                GUI.trocarTela("menu");
+            }
+        }
+        Database.Desconectar();
+
+        /*
+         * else {
+         * webEngine.executeScript("chamarInvalido()");
+         * }
+         */
     }
 }
--- src/main/java/com/alzira/database/Database.java
+++ src/main/java/com/alzira/database/Database.java
@@ -8,33 +8,51 @@ import java.sql.Statement;
 
 public class Database {
     private static String jdbcURL = "jdbc:h2:file:./src/main/resources/DATABASE/database";
-    //src\main\resources\DATABASE
     private static String username = "SA";
     private static String password = "1234";
 
-    public static ResultSet Executar(String sql) throws SQLException {
-        Connection connection = DriverManager.getConnection(jdbcURL, username, password);
+    private static Connection connection;
 
-        //String sql = "SELECT * FROM students";
+    public static Connection Conectar() throws SQLException {
+        if (connection == null) {
+            connection = DriverManager.getConnection(jdbcURL, username, password);
+        }
+        
+        return connection;
+    }
+
+    public static void Desconectar() throws SQLException {
+        if (connection != null) {
+            connection.close();
+            connection = null;
+        }
+    }
+
+    public static ResultSet Ler(String sql) throws SQLException {
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery(sql);
-
-        connection.close();
 
         return resultSet;
     }
 
+    public static void Executar(String sql) throws SQLException {
+        Statement statement = connection.createStatement();
+        statement.executeUpdate(sql);
+    }
+
     /*
+        Connection ab = Database.Conectar();
         Database.Executar("Create table usuarios (username varchar(256) primary key, senha varchar(256))");
         Database.Executar("Insert into usuarios (username, senha) values ('admin', 'admin')");
 
-        ResultSet a = Database.Executar("SELECT * FROM usuarios");
+        ResultSet a = Database.Ler("SELECT * FROM usuarios");
 
-        while (a.next()) {
+        if (a.next()) {
 
-            String username = a.getInt("username");
+            String username = a.getString("username");
             String senha = a.getString("senha");
             System.out.println("Student #" + ": " + username + ", " + senha);
         }
+        Database.Desconectar();
      */
 }

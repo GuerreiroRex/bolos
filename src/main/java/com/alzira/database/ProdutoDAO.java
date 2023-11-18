4e923305f63df5dfb9fac9c46556cb53cd5a38b6
Renan Cesar De Araujo <101188631+RenanCesarDeAraujo@users.noreply.github.com>
2023-11-18 12:30:17 -0300

    inserção da lista de ingrediente no Ler_produtos()

--- src/main/java/com/alzira/database/ProdutoDAO.java
+++ src/main/java/com/alzira/database/ProdutoDAO.java
@@ -1,6 +1,7 @@
 package com.alzira.database;
 
 import com.alzira.model.ProdutoModel;
+import com.alzira.model.ProdutoViewModel;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -23,27 +24,51 @@ public class ProdutoDAO {
     
     public List execSelect() throws JsonProcessingException, SQLException, InterruptedException {
 
-        List<ProdutoModel> lista = new ArrayList<>();
+        List<ProdutoViewModel> lista = new ArrayList<>();
+        //List<String> listaIngrediente = new ArrayList<>();
+        List<Integer> listaID = new ArrayList<>(); //essa lista serve para comparar se o ID vindo do banco já foi inserido na lista que é retornada.
 
         Database.Conectar();
-        ResultSet resultado = Database.Ler("SELECT * FROM produtos");
+        ResultSet resultado = Database.Ler("SELECT * FROM VW_VISUALIZAPRODUTO");
+        
+        
 
         Integer db_id = null;
         String db_nomeProduto = null;
         Integer db_categoriaID = null;
+        String db_nomeCategoria = null;
         Double db_custoIngrediente = null;
         Double db_margemLucro = null;
         Double db_precoFinal = null;
+        String db_nomeIngrediente = null;
 
-        while (resultado.next()) {            
+        while (resultado.next()) {
+            List<String> listaIngrediente = new ArrayList<>();
+            
             db_id = resultado.getInt("produtoid");
+            
+            if(listaID.contains(db_id)){
+                continue;
+            }
+            listaID.add(db_id);
+            
             db_nomeProduto = resultado.getString("nomeproduto");
             db_categoriaID = resultado.getInt("categoriaid");
+            db_nomeCategoria = resultado.getString("nomecategoria");
             db_custoIngrediente = resultado.getDouble("custoingredientes");
             db_margemLucro = resultado.getDouble("margemlucro");
             db_precoFinal = resultado.getDouble("precofinal");
-    
-            lista.add(new ProdutoModel(db_id, db_nomeProduto, db_categoriaID, db_custoIngrediente, db_margemLucro, db_precoFinal));
+
+            
+            ResultSet resultadoAux = Database.Ler("SELECT * FROM VW_VISUALIZAPRODUTO WHERE PRODUTOID = " + db_id);            
+
+            while(resultadoAux.next()){
+                db_nomeIngrediente = resultadoAux.getString("nomeingrediente");
+                listaIngrediente.add(db_nomeIngrediente);
+            }
+            
+            lista.add(new ProdutoViewModel(db_id, db_nomeProduto, db_categoriaID,db_nomeCategoria, db_custoIngrediente, db_margemLucro, db_precoFinal, listaIngrediente));
+            
         }
         
         Database.Desconectar();
---
+++ src/main/java/com/alzira/model/ProdutoViewModel.java
@@ -0,0 +1,95 @@
+package com.alzira.model;
+
+import java.util.List;
+
+/**
+ *
+ * @author Renanzed
+ */
+public class ProdutoViewModel {
+    public Integer produtoID;
+    public String nomeProduto;
+    public Integer categoriaID;
+    public String nomeCategoria;
+    public Double custoIngredientes;
+    public Double margemLucro;
+    public Double precoFinal;
+    public List nomeIngrediente;
+
+    public Integer getProdutoID() {
+        return produtoID;
+    }
+
+    public void setProdutoID(Integer produtoID) {
+        this.produtoID = produtoID;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public Integer getCategoriaID() {
+        return categoriaID;
+    }
+
+    public void setCategoriaID(Integer categoriaID) {
+        this.categoriaID = categoriaID;
+    }
+
+    public String getNomeCategoria() {
+        return nomeCategoria;
+    }
+
+    public void setNomeCategoria(String nomeCategoria) {
+        this.nomeCategoria = nomeCategoria;
+    }
+
+    public Double getCustoIngredientes() {
+        return custoIngredientes;
+    }
+
+    public void setCustoIngredientes(Double custoIngredientes) {
+        this.custoIngredientes = custoIngredientes;
+    }
+
+    public Double getMargemLucro() {
+        return margemLucro;
+    }
+
+    public void setMargemLucro(Double margemLucro) {
+        this.margemLucro = margemLucro;
+    }
+
+    public Double getPrecoFinal() {
+        return precoFinal;
+    }
+
+    public void setPrecoFinal(Double precoFinal) {
+        this.precoFinal = precoFinal;
+    }
+
+    public List getNomeIngrediente() {
+        return nomeIngrediente;
+    }
+
+    public void setNomeIngrediente(List nomeIngrediente) {
+        this.nomeIngrediente = nomeIngrediente;
+    }
+
+    public ProdutoViewModel(Integer c_produtoID, String c_nomeProduto, Integer c_categoriaID, String c_nomeCategoria, Double c_custoIngredientes, Double c_margemLucro, Double c_precoFinal, List c_nomeIngrediente) {
+        this.produtoID = c_produtoID;
+        this.nomeProduto = c_nomeProduto;
+        this.categoriaID = c_categoriaID;
+        this.nomeCategoria = c_nomeCategoria;
+        this.custoIngredientes = c_custoIngredientes;
+        this.margemLucro = c_margemLucro;
+        this.precoFinal = c_precoFinal;
+        this.nomeIngrediente = c_nomeIngrediente;
+    }
+    
+    
+}
